479e710579db8fd3c9141c046ad800a1c628379a
rasdinho <ras.dinho7@gmail.com>
2020-03-10 16:55:51 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,48 @@
+class Dog
+    def initialize (name, breed="Mutt")
+        @name = name 
+        @breed = breed
+    end
+
+    def name= (name) 
+        @name = name 
+        
+    end
+def name 
+    @name 
+end
+
+    def breed=(breed)
+        @breed = breed
+        
+    end 
+    def breed 
+        @breed
+    end 
+end
+
+
+# class Dog 
+
+#     def initialize(name, breed="Mut")
+#       @name = name
+#       @breed = breed
+#     end
+  
+#     def name=(name)
+#       @name = name
+#     end
+  
+#     def name 
+#       @name
+#     end
+  
+#     def breed=(breed)
+#       @breed = breed
+#     end
+  
+#     def breed
+#       @breed
+#     end
+  
+#   end
